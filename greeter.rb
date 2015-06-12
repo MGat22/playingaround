12090428ade044ac75d2458a179ac122f256a82e
MGat22 <zanku2244@yahoo.com>
2015-06-11 22:40:21 -0400

    pushing ruby code

---
+++ greeter.rb
@@ -0,0 +1,4 @@
+puts "Please enter your name:"
+name = gets.chomp
+puts "Hello #{name}! Did you know your name has #{name.length}
+letters in it and written backwards it is #{name.reverse.downcase.capitalize}?"
---
+++ web_greeter.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+
+get '/:name' do
+	name = params[:name]
+	"Hello #{name}! Did you know your name has #{name.length}
+	letters in it and written backwards it is #{name.reverse.downcase.capitalize}?"
+end
